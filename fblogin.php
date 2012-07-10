3836726a0ef34da73a445298771a657346f135c2
byo <byo.spoon@gmail.com>
2012-07-10 20:10:18 +0200

    Enable piwigo sessions before reading facebook's authentication response.

--- fblogin.php
+++ fblogin.php
@@ -3,6 +3,7 @@
 define('PHPWG_ROOT_PATH', '../../');
 
 include_once(PHPWG_ROOT_PATH . 'include/common.inc.php');
+require_once(PHPWG_ROOT_PATH . 'include/functions_session.inc.php' );
 include_once('include/eapfacebook.class.php');
 
 $user = EAPFacebook::grabLoginInfo();
