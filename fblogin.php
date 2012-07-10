20cf60db6322b44bfffa71fc5dab4fb5c9a3b294
byo <byo.spoon@gmail.com>
2012-07-10 21:09:20 +0200

    Introduce EAPUser class

--- fblogin.php
+++ fblogin.php
@@ -4,7 +4,11 @@ define('PHPWG_ROOT_PATH', '../../');
 
 include_once(PHPWG_ROOT_PATH . 'include/common.inc.php');
 require_once(PHPWG_ROOT_PATH . 'include/functions_session.inc.php' );
+include_once('include/eapuser.class.php');
 include_once('include/eapfacebook.class.php');
 
 $user = EAPFacebook::grabLoginInfo();
-var_dump( $user );
+if ( $user === FALSE ) die( 'Error' );
+
+EAPUser::processAuthentication( 'FACEBOOK', $user['id'], array(
+));
---
+++ include/eapuser.class.php
@@ -0,0 +1,12 @@
+<?php
+
+// Chech whether we are indeed included by Piwigo.
+if (!defined('PHPWG_ROOT_PATH')) die('Hacking attempt!');
+
+class EAPUser
+{
+
+	public static function processAuthentication( $hostPlatform, $hostPlatformId, $userInfo )
+	{
+	}
+}
