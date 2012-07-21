5304728a8a57bf1089908304804396c628ddef56
byo <byo.spoon@gmail.com>
2012-07-21 12:00:40 +0200

    Add GPL headeres

--- admin_config.php
+++ admin_config.php
@@ -1,6 +1,26 @@
 <?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - external authentication plugin                               |
+// |                                 https://github.com/byo/piwigo-extauth |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2012 Bartlomiej (byo) wiecki                             |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation; either version 2 of the License, or     |
+// | (at your option) any later version.                                   |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
 
-// Check whether we are indeed included by Piwigo.
 if (!defined('PHPWG_ROOT_PATH')) die('Hacking attempt!');
 
 include_once('include/eapbase.class.php');
--- main.inc.php
+++ main.inc.php
@@ -2,12 +2,33 @@
 /*
 Version: 1.0
 Plugin Name: External authentications plugin
-Plugin URI:
-Author: B.Swiecki
+Plugin URI: https://github.com/byo/piwigo-extauth
+Author: Bartlomiej (byo) Swiecki
 Description: This plugin enable users to login using etxternal authentication methods such as facebook or google.
 */
 
-// Chech whether we are indeed included by Piwigo.
+// +-----------------------------------------------------------------------+
+// | Piwigo - external authentication plugin                               |
+// |                                 https://github.com/byo/piwigo-extauth |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2012 Bartlomiej (byo) wiecki                             |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation; either version 2 of the License, or     |
+// | (at your option) any later version.                                   |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
 if (!defined('PHPWG_ROOT_PATH')) die('Hacking attempt!');
 
 define( 'EAP_PATH', realpath( PHPWG_PLUGINS_PATH.basename(dirname(__FILE__)) ).'/' );
