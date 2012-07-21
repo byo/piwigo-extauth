ae1877b017fbb7d3d544b1d0246d47fad5c9c316
byo <byo.spoon@gmail.com>
2012-07-21 12:13:38 +0200

    Display separate message when there are no accounts waiting for the merge

--- language/pl_PL/plugin.lang.php
+++ language/pl_PL/plugin.lang.php
@@ -12,3 +12,6 @@ $lang['Connect with:'] = "Zaloguj poprzez:";
 
 $lang['Approval pending'] = "Walidacja w toku";
 $lang['Thank you for stepping by. Administrators will approve your account soon.'] = "Dziękujemy za odwiedziny. Administratorzy aktywują wkrótce Twoje konto.";
+
+$lang['There are no account waiting for association. Ask your users to login using external method in order to associate them with the system.'] = "W chwili obecnej nie ma kont oczekujących na połączenie. Poproś użytkowników by skorzystali z autentykacji zewnętrznej by połączyć ich konta.";
+
